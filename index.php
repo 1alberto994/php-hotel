6a11944c4697c0fe26d38694ad5a3386e10eb7c0
1alberto994 <alby94ct@gmail.com>
2023-02-20 14:47:44 +0100

    inizio progetto

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+$hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+    $title='PHP HOTEL';
+    
+        foreach($hotels as $hotel){
+           echo '<table >'
+           echo '<tr>'
+            echo'<th>' echo "$hotel[name]" echo'</th>'
+            
+            
+            echo'</tr>'
+           echo '<tr>'
+           echo '<td>' <?php echo "$hotel['description']" ?>'</td>'
+            
+            </tr>
+            <tr>
+            <td><?php echo $hotel['vote'];?></td>
+            
+            </tr>
+        </table>
+        }
+           
+                
+                
+            ;
+    
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP HOTEL</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    
+</head>
+<body>
+    <h1><?php echo $title?></h1>
+    
+        
+
+</body>
+</html>
